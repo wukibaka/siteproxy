33b708616fca93796fc5e649adb0ac33d77e75d0
lz差不多是条咸鱼了 <lz23333@outlook.com>
2022-04-05 17:17:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-nu.vercel.app',
+    serverName: 'siteproxy-233.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
